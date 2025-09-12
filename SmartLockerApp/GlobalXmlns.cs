b2a5106de2aa8cef4e39b630b3500011ddbbe14d
nolan-rey <nolan.reymond@saintmichelannecy.fr>
2025-09-12 15:59:01 +0200

    Ajoutez des fichiers projet.

---
+++ SmartLockerApp/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "SmartLockerApp")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "SmartLockerApp.Pages")]
